6f59057fce0bed220a39d83ef8d66a63d4873ca2
AmyLipscomb <amy.a.lipscomb@gmail.com>
2023-01-19 08:27:16 -0500

    added initial file structure

---
+++ config/connection.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+// Wrap Mongoose around local connection to MongoDB
+// Name of the database is "socialNetwork"
+mongoose.connect('mongodb://127.0.0.1:27017/socialNetworkDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+// Export connection 
+module.exports = mongoose.connection;
+
+
+
+//Below is the connection.js code for if Heroku was being used 
+    // const { connect, connection } = require('mongoose');
+
+    // // After you create your Heroku application, visit https://dashboard.heroku.com/apps/ select the application name and add your Atlas connection string as a Config Var
+    // // Node will look for this environment variable and if it exists, it will use it. Otherwise, it will assume that you are running this application locally
+    // const connectionString =
+    //   process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialNetworkDB';
+
+    // connect(connectionString, {
+    //   useNewUrlParser: true,
+    //   useUnifiedTopology: true,
+    // });
+
+    // module.exports = connection;
