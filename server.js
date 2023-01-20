662f7115b84706b452fe62f58f436a7b96f0d719
AmyLipscomb <amy.a.lipscomb@gmail.com>
2023-01-20 09:03:19 -0500

    added coding to the server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+// Run npm install mongodb and require mongodb and MongoClient class
+  //express will create the server and mongodb will talk to the database
+const express = require('express');
+const db = require('./config/connection');
+const routes = require('./routes');
+
+const cwd = process.cwd();
+
+//server configuration 
+const PORT = process.env.port || 3001;
+const app = express();
+
+//installing middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+
+// Runs the server
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server for ${activity} running on port ${PORT}!`);
+  });
+});
