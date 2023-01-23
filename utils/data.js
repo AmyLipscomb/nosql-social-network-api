73abf06cb0e38ce402af4dd13b94db887fe844e6
AmyLipscomb <amy.a.lipscomb@gmail.com>
2023-01-23 08:16:38 -0500

    Added seed data

---
+++ utils/data.js
@@ -0,0 +1,94 @@
+
+const userData = [
+    {
+        username: "Amy",
+        email: "amy.a.lipscomb@gmail.com",
+       thoughts:  "First thought on the social network api!",
+       friends: ["Jake, Audrey, Bobbie, Kovu, Nari, Maddie, Josh, Joey"]
+
+    },
+
+    {
+        username: "Jake",
+        email: "jake.k@gmail.com",
+        thoughts: "Nothing like a rainy Monday...",
+        friends: ["Amy, Audrey, Josh, Joey, Cleo, Bobbie, Maddie"]
+    },
+
+    {
+        username: "Bobbie",
+        email: "bobbie.lip@gmail.com",
+        thoughts: "Hi!",
+        friends: ["Amy, Audrey, Jake, Nari"]
+    },
+    {
+        username: "Audrey",
+        email: "audrey.g@gmail.com",
+        thoughts: "My first thought!",
+        friends: ["Amy, Bobbie, Jake, Kovu"]
+    },
+
+    {
+        username: "Josh",
+        email: "josh.k@gmail.com",
+        thoughts: "Hey",
+        friends:["Jake, Maddie, Joey, Cleo, Amy"],
+    },
+
+    {
+        username: "Nari" ,
+        email: "nari.g@gmail.com",
+        thoughts: "Orange tabby cats are the best!",
+        friends: ["Bobbie, Amy, Kovu, Jake"]
+    },
+    {
+        username: "Kovu",
+        email: "kovu.g@gmail.com",
+        thoughts:"I love string!",
+        friends: ["Audrey, Nari, Jake, Amy, Bobbie"]
+    },
+
+    {
+        username: "Maddie",
+        email: "Maddie@gmail.com",
+        thoughts: "Hellooooo",
+        friends: ["Josh, Jake, Amy, Joey"]
+    },
+
+    {
+        username: "Cleo",
+        email: "Cleo.k@gmail.com",
+        thoughts: "I love fetch!",
+        friends: ["Jake, Josh, Joey, Amy, Maddie"],
+    },
+
+    {
+        username: "Joey",
+        email: "joey.r@gmail.com",
+        thoughts: "Hello!",
+        friends: ["Jake, Josh, Cleo, Amy, Maddie"]
+    },
+];
+
+const seedUser = () => User.bulkCreate(userData,{
+    individualHooks: true,
+    returning: true
+});
+
+module.exports = seedUser;
+
+
+// const userNames = [
+//     "Amy", - added
+//     "Bobbie", - added
+//     "Jake", - added
+//     "Audrey", - added
+//     "Josh", - added
+//     "Nari", - added
+//     "Kovu", - added
+//     "Maddie", - added
+//     "Cleo", - added
+//     "Joey" - added
+// ];
+
+
