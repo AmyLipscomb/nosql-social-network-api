b5d24e844c4e8813f5828dfc33cd7bc22804bf43
AmyLipscomb <amy.a.lipscomb@gmail.com>
2023-01-23 07:30:43 -0500

    added more coding to userRoute

--- routes/api/userRoute.js
+++ routes/api/userRoute.js
@@ -7,8 +7,14 @@ const {
     deleteUser
 } = require("../../controllers/userController");
     
-
+// /api/users
 router.route("/").get(getUsers).post(createUser)
 
+// /api/users/: userId
+router
+    .route("/:userId")
+    .get(getSingleUser)
+    .put(updateUser)
+    .delete(deleteUser);
 
 module.exports = router;
