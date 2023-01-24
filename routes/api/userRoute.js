2bc0fed90604893a95052a1894558f4a51dbcaf7
AmyLipscomb <amy.a.lipscomb@gmail.com>
2023-01-23 22:46:25 -0500

    added thoughtController and thoughtRoute, edited the index.js route to include the thoughtRoute

--- routes/api/userRoute.js
+++ routes/api/userRoute.js
@@ -10,7 +10,10 @@ const {
 // /api/users
 router.route("/").get(getUsers).post(createUser)
 
-// /api/users/: userId
+// http://localhost:3001/api/users/63ce9185b03d0e08ccad91aa
+// /api/users/: userId <- In Insomnia, after  ' users/ ' you'll whatever ID you're looking for- in the link above, I'm pulling up 'Amy' . You don't need the ' : '
+    //In Insomnia, I changed 'Amy' to 'Cleo'. It works, and then when I do a 'GET' to GET all of the users, I saw that 'Amy' changed to 'Cleo' 
+
 router
     .route("/:userId")
     .get(getSingleUser)
